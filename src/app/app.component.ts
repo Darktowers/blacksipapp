6d68a994ce0ad9f21dfdfbaf04aa24440a483e32
Cristian <cristian@150porciento.com>
2017-11-17 00:17:28 -0500

    Actualizacion

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,29 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'app';
+  title = 'My app yiahh';
+  nombreServicio = "";
+  clickServicio = false;
+  selectedItem = "" 
+  lugares:any = [
+    { cercania:1, active:true, nombre: 'Floreria'},
+    { cercania:2, active:false, nombre: 'Donas'},
+    { cercania:1, active:true, nombre: 'Veterinaria'},
+    { cercania:3, active:true, nombre: 'Veterinaria'}
+    
+  ];
+  mostrarForm(nombre,event:any){
+    console.log(event);
+    this.nombreServicio = nombre;
+    this.clickServicio = true;
+    this.selectedItem = nombre;
+    return false;
+  }
+  constructor(){
+
+  }
+
 }
